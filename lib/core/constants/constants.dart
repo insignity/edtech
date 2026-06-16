0ce23aea3e15f753b05c9c4105222e1386178ada
Aiarsien Diachkovskii <ayarsen1995@gmail.com>
2026-06-15 23:32:29 -0400

    api

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,4 @@
 abstract class Constants {
   static const String myapp = "prolingua";
-  static const String url = "http://34.237.152.83:8000/api";
+  static const String url = "https://api.lingloop.org/api/";
 }
